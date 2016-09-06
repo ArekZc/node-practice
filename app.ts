b989c949e95528cf58912f7a56f6a7c545454a47
Arek Zając <a.zajac@clearcode.cc>
2016-09-06 18:50:55 +0200

    move javascript files to typescript

--- app.ts
+++ app.ts
@@ -5,9 +5,6 @@ const logger = require("morgan");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 
-const routes = require("./routes/index");
-const users = require("./routes/users");
-
 const app = express();
 
 // view engine setup
@@ -22,15 +19,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", routes);
-app.use("/users", users);
+app.get("/", (req, res) => res.json({hello: "World"}));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  let err: any = new Error("Not Found");
+app.use(function (req, res, next) {
+    let err: any = new Error("Not Found");
 
-  err.status = 404;
-  next(err);
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -38,23 +34,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get("env") === "development") {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render("error", {
-      message: err.message,
-      error: err
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render("error", {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render("error", {
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render("error", {
+        message: err.message,
+        error: {}
+    });
 });
 
 
