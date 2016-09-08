2760cef34ea915042f82e22a17abc1ca34d8fc4a
Arek Zając <a.zajac@clearcode.cc>
2016-09-09 00:23:50 +0200

    #21 add typescript decorators for express

--- app.ts
+++ app.ts
@@ -1,57 +1,53 @@
-const express = require("express");
-const path = require("path");
-const favicon = require("serve-favicon");
-const logger = require("morgan");
-const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
+import * as Express from "express";
+import {ServerLoader} from "ts-express-decorators/server-loader";
+import Path = require("path");
 
-const app = express();
+export class Server extends ServerLoader {
+    constructor() {
+        super();
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+        let appPath: string = Path.resolve(__dirname);
 
-// uncomment after placing your favicon in /public
-// app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
-app.use(logger("dev"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+        this.setEndpoint("/v1")
+            .scan(appPath + "/controllers/**/**.js")
+            .createHttpServer(3000)
+            .createHttpsServer({
+                port: 3030
+            });
 
-app.get("/", (req, res) => res.json({hello: "World"}));
+    }
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    let err: any = new Error("Not Found");
+    public importMiddlewares(): Server {
+        let morgan = require("morgan"),
+            cookieParser = require("cookie-parser"),
+            bodyParser = require("body-parser"),
+            compress = require("compression"),
+            methodOverride = require("method-override"),
+            session = require("express-session");
 
-    err.status = 404;
-    next(err);
-});
+        this
+            .use(morgan("dev"))
+            .use(ServerLoader.AcceptMime("application/json"))
+            .use(bodyParser.json())
+            .use(bodyParser.urlencoded({
+                extended: true
+            }))
+            .use(cookieParser())
+            .use(compress({}))
+            .use(methodOverride());
 
-// error handlers
+        return this;
+    }
 
-// development error handler
-// will print stacktrace
-if (app.get("env") === "development") {
-    app.use(function (err, req, res, next) {
-        res.status(err.status || 500);
-        res.render("error", {
-            message: err.message,
-            error: err
-        });
-    });
+    static Initialize(): Promise<any> {
+
+        console.log("Initialize server");
+
+        return new Server()
+            .start()
+            .then(() => {
+                console.log("Server started...");
+            });
+    }
+
 }
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render("error", {
-        message: err.message,
-        error: {}
-    });
-});
-
-
-module.exports = app;
